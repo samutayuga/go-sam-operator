77d6f0f64c6655fbcf2f0fa1a23195c0247e0068
putumas <putumas.mertayasa.e@thalesdigital.io>
2021-11-14 21:52:46 +0800

    chore: put option to use incluster and outofcluster config

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,29 @@
 # Build the manager binary
 FROM golang:1.17 as builder
 
-WORKDIR /workspace
+WORKDIR /hook
 # Copy the Go Modules manifests
 COPY go.mod go.mod
 COPY go.sum go.sum
+COPY pod_displayer.gotmpl pod_displayer.gotmpl
+COPY sample.gotmpl sample.gotmpl
 # cache deps before building and copying source so that we don't need to re-download as much
 # and so that source changes don't invalidate our downloaded layer
 RUN go mod download
 
 # Copy the go source
 COPY main.go main.go
+ENV KUBE_NAMESPACE=default
+ENV IS_IN_CLUSTER_DEP=true
 
 # Build
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o hook main.go
 
 # Use distroless as minimal base image to package the manager binary
 # Refer to https://github.com/GoogleContainerTools/distroless for more details
-FROM bitnami/kubectl:1.20
-WORKDIR /
-COPY --from=builder /workspace/hook .
+#FROM bitnami/kubectl:1.20
+#WORKDIR /hook
+#COPY --from=builder /workspace/ .
 USER 65532:65532
 
-ENTRYPOINT ["/hook"]
+CMD ["./hook"]
--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 )
 
@@ -23,39 +26,45 @@ var (
 
 func main() {
 	fmt.Println("hello")
-	namespace := flag.Arg(0)
+
+	namespace := os.Getenv("KUBE_NAMESPACE")
 	var timeOut = int64(30)
 	if namespace != "" {
 		log.Printf("Hook is triggered for the namespace %s\n", namespace)
 		monitor(namespace, &timeOut)
 	} else {
-		log.Println("The hook is invoked without namespace argument")
+		log.Println("The hook.sh is invoked without namespace argument")
 	}
 
 }
 func monitor(namespace string, timeOut *int64) {
 	log.Printf("Hook is triggered for the namespace %s and timeout for watching an event %d seconds\n", namespace, *timeOut)
-	watch, errWatch := clientset.CoreV1().Pods(namespace).Watch(context.TODO(), metav1.ListOptions{Watch: true, TimeoutSeconds: timeOut})
-	if errWatch != nil {
-		log.Printf("No changes of the resources after %d waiting %v\n", timeOut, errWatch)
-		if pods, errList := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{}); errList == nil {
-			if len(pods.Items) > 0 {
-				DisplayPodList(pods.Items)
-			}
-		}
-
-	} else {
-		go func() {
-			for anEvent := range watch.ResultChan() {
-				if p, ok := anEvent.Object.(*v1.Pod); ok {
-					DisplayAPod(p)
-				} else {
-					log.Fatal("unexpected type")
+	if clientset != nil {
+		watch, errWatch := clientset.CoreV1().Pods(namespace).Watch(context.TODO(), metav1.ListOptions{Watch: true, TimeoutSeconds: timeOut})
+		if errWatch != nil {
+			log.Printf("No changes of the resources after %d waiting %v\n", timeOut, errWatch)
+			if pods, errList := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{}); errList == nil {
+				if len(pods.Items) > 0 {
+					DisplayPodList(pods.Items)
 				}
 			}
-		}()
+
+		} else {
+			go func() {
+				for anEvent := range watch.ResultChan() {
+					if p, ok := anEvent.Object.(*v1.Pod); ok {
+						DisplayAPod(p)
+					} else {
+						log.Fatal("unexpected type")
+					}
+				}
+			}()
+		}
+		time.Sleep(5 * time.Second)
+	} else {
+		log.Printf("Cannot call kubernetes api because client set is null")
 	}
-	time.Sleep(5 * time.Second)
+
 }
 
 //InitTemplate ...
@@ -77,9 +86,16 @@ func DisplayAPod(pod *v1.Pod) {
 		log.Printf("error while executing template %s for data %v %v", "pod_displayer.gotmpl", pod, err)
 	}
 }
-func init() {
-	InitTemplate()
-	fmt.Println("this is initialization")
+func inClusterConfig() *rest.Config {
+	if config, err := rest.InClusterConfig(); err == nil {
+		return config
+	} else {
+		log.Printf("Error while calling rest.InClusterConfig(), %v\n", err)
+	}
+	return nil
+
+}
+func outClusterConfig() *rest.Config {
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -89,10 +105,52 @@ func init() {
 	}
 	flag.Parse()
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
+	if config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig); err != nil {
+
+		log.Printf("Error while calling clientcmd.BuildConfigFromFlags, %v\n", err)
+		return nil
+	} else {
+		return config
 	}
-	clientset, err = kubernetes.NewForConfig(config)
+}
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	if currFolder, errGetFolder := os.Getwd(); errGetFolder == nil {
+		log.Printf("Current folder %v\n", currFolder)
+		if files, errReadDir := ioutil.ReadDir(currFolder); errReadDir == nil {
+			for _, aFile := range files {
+				log.Printf("File %v\n", aFile.Name())
+			}
+		} else {
+			log.Panicf("error while getting listing current folder %v\n", errReadDir)
+		}
+	} else {
+		log.Panicf("error while getting current folder %v\n", errGetFolder)
+	}
+	InitTemplate()
+
+	inClusterDeployment := os.Getenv("IS_IN_CLUSTER_DEP")
+	log.Printf("this is initialization with IS_IN_CLUSTER_DEP=%v\n", inClusterDeployment)
+	var kubeConf *rest.Config
+	if inClusterDeployment == "" {
+		kubeConf = outClusterConfig()
+	} else {
+		if isInCluster, errParse := strconv.ParseBool(inClusterDeployment); errParse == nil {
+			if isInCluster {
+				kubeConf = inClusterConfig()
+			} else {
+				kubeConf = outClusterConfig()
+			}
+
+		} else {
+			log.Printf("error while converting IS_IN_CLUSTER_DEP, %v\n", errParse)
+		}
+	}
+	if kubeConf != nil {
+		var clientSetCreateErr error
+		if clientset, clientSetCreateErr = kubernetes.NewForConfig(kubeConf); clientSetCreateErr != nil {
+			log.Fatalf("Error while creating the connection %v\n", clientSetCreateErr)
+		}
+	}
 
 }
